0fd38ebc7bef900a43e5d774a64495510abca9ce
Christopher House <chrishouse180@gmail.com>
2025-03-28 18:52:46 -0700

    init commit

---
+++ madLibs.js
@@ -0,0 +1,20 @@
+const place = 'warehouse';
+const innerPlace = 'robot factory';
+
+const robot = {
+    name: 'RoboMax',
+    dialogue: 'Access Granted!',
+}
+
+const num1 = 8;
+const num2 = 2;
+const mathResult = 8/2;
+
+const isFascinating = true;
+const booleanValue = isFascinating ? 'Fascinating, right?' : "Not that fascinating, right?";
+
+
+const story = `In a hidden ${place}, I stumbled upon a hidden ${innerPlace}. The shiny machines were assembling gadgets endlessly. One robot, named ${robot.name}, handed me a metallic keycard. It beeped and said, ${robot.dialogue} Did you know ${num1} ÷ ${num2} equals ${mathResult}? ${booleanValue}`
+
+document.getElementById("madlibs-output").textContent = story;
+console.log(story);
